9b98b7c89414ccdc495da28df60c39c1e630dc70
klyaist <54140207+klyaist@users.noreply.github.com>
2026-05-29 10:30:26 +0200

    Implement bearing and distance calculations in add_landmark_measurement function

--- src/add_landmark_measurement.py
+++ src/add_landmark_measurement.py
@@ -9,7 +9,16 @@ MEASUREMENT_NOISE = gtsam.noiseModel.Diagonal.Sigmas(np.array([0.05, 0.1]))  # (
 
 def add_landmark_measurement(graph, initial_estimate, result):
     # Determine the correct rotation (bearing) and distance from X(4) to L(2) 
-    # rotation = 
-    # distance = 
-    graph.add(gtsam.BearingRangeFactor2D(X(4), L(2), gtsam.Rot2.fromDegrees(rotation), distance, MEASUREMENT_NOISE))
+    
+    X4 = result.atPose2(X(4))
+    L2 = result.atPoint2(L(2))
+
+    dx = L2.x() - X4.x()
+    dy = L2.y() - X4.y()
+
+    distance = math.hypot(dx, dy)
+
+    rotation = math.atan2(dy, dx) - X4.theta()
+
+    graph.add(gtsam.BearingRangeFactor2D(X(4), L(2), gtsam.Rot2.fromRadians(rotation), distance, MEASUREMENT_NOISE))
     return graph
